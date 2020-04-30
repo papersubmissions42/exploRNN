6379546e0246db19594f79934383887d776d140b
Paper Submissions <papersubmissions42@gmail.com>
2020-04-30 15:01:13 +0200

    VIS Submission

---
+++ precache-manifest.aceacaa449c5ac1c1f86536a14d23e06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e91f9423d177b705477b31e0f2fba15",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a7174070c9230ac05636",
+    "url": "./static/css/main.0ac59006.chunk.css"
+  },
+  {
+    "revision": "ccd6500a0911c1850d7f",
+    "url": "./static/js/2.ec735089.chunk.js"
+  },
+  {
+    "revision": "d5a02c7d165334542a4130210b774dc6",
+    "url": "./static/js/2.ec735089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7174070c9230ac05636",
+    "url": "./static/js/main.2693f871.chunk.js"
+  },
+  {
+    "revision": "e60c553c9a7f5d0aaf3d",
+    "url": "./static/js/runtime-main.89bb6169.js"
+  }
+]);
